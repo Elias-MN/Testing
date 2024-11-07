238faa4e4bb15661fed5591ac238bafa566544ae
EliasMN <elias010493@gmail.com>
2024-11-07 19:52:19 +0100

    Added animation and jump sound

--- src/scene_01.js
+++ src/scene_01.js
@@ -16,6 +16,9 @@ export default class Scene_01 extends Phaser.Scene {
 
     // Load player
     this.load.atlas("player", "assets/squirtle.png", "assets/squirtle.json");
+
+    // Load music
+    this.load.audio("jump", "assets/jump.mp3");
   }
 
   // Runs once, after all assets in preload are loaded
@@ -35,13 +38,61 @@ export default class Scene_01 extends Phaser.Scene {
     map.createLayer("Elements_Layer", tilesetTile);
 
     const floor = map.createLayer("Floor_Layer", tilesetTile);
-    map.createLayer("Ground_Layer", tilesetTile);
+    // This is to make everything collide with the floor
+    floor.setCollisionByExclusion([-1], true);
 
     // Create player
     this.player = this.physics.add.sprite(100, 100, "player");
     this.player.setCollideWorldBounds(true);
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.physics.add.collider(this.player, floor);
+    this.jump_sound = this.sound.add("jump", { volume: 0.5, loop: false });
+
+    this.anims.create({
+      key: "move",
+      // The prefix of 'move_01, move_02...' is 'move_'
+      // end is the last number 'move_01 to move_04': 4
+      // zeroPad is the number of elements '00, 01, 02...': 2
+      frames: this.anims.generateFrameNames("player", {
+        prefix: "move_",
+        start: 1,
+        end: 3,
+        zeroPad: 2,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "idle",
+      frames: [{ key: "player", frame: "idle" }],
+      frameRate: 1,
+      repeat: -1,
+    });
+
+    map.createLayer("Ground_Layer", tilesetTile);
   }
 
   // Runs once per frame for the duration of the scene
-  update() {}
+  update() {
+    // Player movement
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-50);
+      this.player.setFlipX(true);
+      this.player.anims.play("move", true);
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(50);
+      this.player.setFlipX(false);
+      this.player.anims.play("move", true);
+    } else {
+      this.player.setVelocityX(0);
+      this.player.anims.play("idle", true);
+    }
+
+    // Player jump
+    if (this.cursors.up.isDown && this.player.body.onFloor()) {
+      this.player.setVelocityY(-250);
+      this.jump_sound.play();
+    }
+  }
 }
